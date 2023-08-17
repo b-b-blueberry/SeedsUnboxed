461619ba7b18cecf59d5320a7f8dbeca2c1c337f
blueberry <dearbblueberry@gmail.com>
2023-08-17 18:08:01 +1000

    Update for SMAPI 4:
    - Use content events, replacing IAssetEditor.
    - Add slightly more safety around asset edits.

--- SeedsUnpacked/ModEntry.cs
+++ SeedsUnpacked/ModEntry.cs
@@ -1,12 +1,12 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Linq;
+using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewValley;
-using System;
-using System.Linq;
 
 namespace SeedsUnpacked
 {
-	public class ModEntry : Mod, IAssetEditor
+	public sealed class ModEntry : Mod
 	{
 		private static readonly string[] InvalidSeeds =
 		{
@@ -19,37 +19,70 @@ namespace SeedsUnpacked
 			"Sunflower",
 		};
 
+		const string AssetKey = @"Maps/springobjects";
+
 		public override void Entry(IModHelper helper)
 		{
-			Helper.Events.GameLoop.SaveLoaded += delegate { Helper.Content.InvalidateCache(@"Maps/springobjects"); };
+			this.Helper.Events.Content.AssetRequested += this.OnAssetRequested;
+			this.Helper.Events.GameLoop.SaveLoaded += this.OnSaveLoaded;
 		}
 
-		public bool CanEdit<T>(IAssetInfo asset)
+		private void OnSaveLoaded(object sender, StardewModdingAPI.Events.SaveLoadedEventArgs e)
 		{
-			return Game1.objectInformation != null && asset.AssetNameEquals(@"Maps/springobjects");
+			this.Helper.GameContent.InvalidateCache(ModEntry.AssetKey);
 		}
 
-		public void Edit<T>(IAssetData asset)
+		private void OnAssetRequested(object sender, StardewModdingAPI.Events.AssetRequestedEventArgs e)
 		{
-			foreach (var id in Game1.objectInformation.Keys.Where(id => Game1.objectInformation[id].Split('/') is string[] split
-				&& (split[0].ToLower().EndsWith("seeds") || split[0].ToLower().EndsWith("bulb")) // No starters or saplings (stage 0 is taller than 16px)
-				&& InvalidSeeds.All(prefix => !split[0].StartsWith(prefix)) // No non-standard (multiple possible harvest) seeds
-				&& !split[5].EndsWith("trellis."))) // No trellis seeds (stage 0 is taller than 16px)
+			if (Game1.objectInformation is not null && Game1.cropSpriteSheet is not null && e.NameWithoutLocale.IsEquivalentTo(ModEntry.AssetKey))
 			{
-				try
+				e.Edit(apply: this.Edit);
+			}
+		}
+
+		private void Edit(IAssetData asset)
+		{
+			try
+			{
+				foreach (int id in Game1.objectInformation.Keys.Where((int id) => Game1.objectInformation[id].Split('/') is string[] split
+					// No starters or saplings (stage 0 is taller than 16px):
+					&& (split[0].ToLower().EndsWith("seeds") || split[0].ToLower().EndsWith("bulb"))
+					// No non-standard (multiple possible harvest) seeds:
+					&& ModEntry.InvalidSeeds.All((string prefix) => !split[0].StartsWith(prefix))
+					// No trellis seeds (stage 0 is taller than 16px):
+					&& !split[5].EndsWith("trellis.")))
 				{
-					var crop = new Crop(id, 0, 0);
-					var sourceArea = Helper.Reflection.GetMethod(crop, "getSourceRect").Invoke<Rectangle>(0);
-					sourceArea.Height = 16;
-					sourceArea.Y += sourceArea.Height;
-					var targetArea = Game1.getSourceRectForStandardTileSheet(Game1.objectSpriteSheet, id, sourceArea.Width, sourceArea.Height);
-					asset.AsImage().PatchImage(Game1.cropSpriteSheet, sourceArea, targetArea, PatchMode.Replace);
+					try
+					{
+						Crop crop = new(seedIndex: id, tileX: 0, tileY: 0);
+
+						Rectangle source = crop.getSourceRect(number: 0);
+						source.Y += source.Height - Game1.smallestTileSize;
+						source.Height = Game1.smallestTileSize;
+						source.Width = Game1.smallestTileSize;
+
+						Rectangle target = Game1.getSourceRectForStandardTileSheet(
+							tileSheet: Game1.objectSpriteSheet,
+							tilePosition: id,
+							width: source.Width,
+							height: source.Height);
+
+						asset.AsImage().PatchImage(
+							source: Game1.cropSpriteSheet,
+							sourceArea: source,
+							targetArea: target,
+							patchMode: PatchMode.Replace);
+					}
+					catch (Exception e)
+					{
+						this.Monitor.Log($"Error editing crop {id}:{Environment.NewLine}{e}", LogLevel.Error);
+						continue;
+					}
 				}
-				catch (Exception e)
-				{
-					Monitor.Log($"{e}", LogLevel.Error);
-					continue;
-				}
+			}
+			catch (Exception e)
+			{
+				this.Monitor.Log($"Error editing crops. Some sprites may not be changed.{Environment.NewLine}{e}", LogLevel.Error);
 			}
 		}
 	}
